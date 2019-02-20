a0d38a3156ea9603789d5272ec48487f380199e8
Rachel Wyatt <wyattr@gmail.com>
2019-02-19 23:12:15 -0500

    added some stuff to search page - doesn't do anything yet though

--- coop-housing/src/app/housing-search/housing-search.component.ts
+++ coop-housing/src/app/housing-search/housing-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Housing } from '../housing';
 
 @Component({
   selector: 'app-housing-search',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./housing-search.component.css']
 })
 export class HousingSearchComponent implements OnInit {
-
+  private housingList: Array<Housing> = [
+    {id:1, name:'Rental Co 1', street:'123 Main St.', city:'Detroit', state:'MI', zip:12345, coop:'GM', rent:500},
+    {id: 2, name:'Rental Co 2', street:'459 Maple Ave.', city:'Lansing', state:'MI', zip:54321, coop:'State of Michigan', rent: 700}
+  ];
   constructor() { }
 
   ngOnInit() {
